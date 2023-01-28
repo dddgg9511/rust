844dbfc74e1d09691ee61a0e7cac3abbe5671525
Choo <ddgg9511@naver.com>
2023-01-28 17:28:23 +0900

    Update main.rs

--- fd/src/main.rs
+++ fd/src/main.rs
@@ -25,3 +25,21 @@ fn run() -> Result<ExitCode> {
 
     ExitCode::Success.exit();
 }
+
+fn set_working_dir(opts: &Opts) -> Result<()> {
+    if let Some(ref base_directory) = opts.base_directory {
+        if !filesystem::is_existing_directory(base_directory) {
+            return Err(anyhow!(
+                "The '--base-directory' path '{}' is not a directory.",
+                base_directory.to_string_lossy()
+            ));
+        }
+        env::set_current_dir(base_directory).with_context(|| {
+            format!(
+                "Could not set '{}' as the current working directory",
+                base_directory.to_string_lossy()
+            )
+        })?;
+    }
+    Ok(())
+}
